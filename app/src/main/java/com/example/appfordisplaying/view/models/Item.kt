ddc42c320406d5e86e800638f3a286813b6884f1
Roman Barmutov <barmutovroman@gmail.com>
2022-02-05 17:30:15 +0200

    Big update (add koin)

--- app/src/main/java/com/example/appfordisplaying/view/models/Item.kt
+++ app/src/main/java/com/example/appfordisplaying/view/models/Item.kt
@@ -6,6 +6,6 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class Item(
     @Json(name = "id") val id: Int,
+    @Json(name = "title") val title: String,
     @Json(name = "url") val url: String,
-    @Json(name = "description") val description: String
 )
--- app/src/main/java/com/example/appfordisplaying/view/viewModels/FragmentViewModel.kt
+++ app/src/main/java/com/example/appfordisplaying/view/viewModels/FragmentViewModel.kt
@@ -4,19 +4,22 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.appfordisplaying.view.repository.ListUseCase
 import com.example.appfordisplaying.view.models.Item
-import com.example.appfordisplaying.view.repository.ListRepository
 import kotlinx.coroutines.launch
 
-class ListViewModel : ViewModel() {
-    private val listRepository: ListRepository = TODO()
-
+class FragmentViewModel(private val listUseCase: ListUseCase): ViewModel() {
     private val _itemList = MutableLiveData<List<Item>>()
     val itemList: LiveData<List<Item>> = _itemList
 
-    fun loadList() {
+    fun loadItemList() {
         viewModelScope.launch {
-            _itemList.value = listRepository.getItemList()
+            try {
+                _itemList.value = listUseCase.loadItemList()
+            }
+            catch (e: Exception){
+                _itemList.value = listOf(Item(0, "${e.message}",""))
+            }
         }
     }
 }
